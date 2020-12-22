d7a7ff6eec68b04ea935f68d90e900ae6c66b4f1
Yasin <ysndlklc1989@icloud.com>
2020-12-22 13:06:48 +0300

    loginVC added

---
+++ OdeonDynamics/Controllers/LoginViewController.swift
@@ -0,0 +1,18 @@
+//
+//  LoginViewController.swift
+//  OdeonDynamics
+//
+//  Created by Yasin Dalkilic on 22.12.2020.
+//  Copyright © 2020 Azure. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
